b9786abc4af2e5c15a61868330af35f15ce3d5d5
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-05-03 23:59:46 -0300

    added pipenv and some new links where to get data

--- pytest.py
+++ pytest.py
@@ -1,26 +1,33 @@
-import owslib
-from owslib.feature.wfs200 import ContentMetadata
-from owslib.wfs import WebFeatureService
 import os
 import shutil
 
+from owslib.wfs import WebFeatureService
+
+# Datos a obtener:
+# Escuelas, universidades, hospitales y centros de salud, policía y bomberos
+# Transporte público, rutas, autopistas, puertos y aeropuertos
+
 # wfs_url = 'http://mapa.educacion.gob.ar/geoserver/ows?service=wfs&version=1.1.0&request=GetCapabilities'
+
+# Transport data
+# Colectivos - Estaciones - Puertos - Ferrocarril - Subtes - Ruta Nacional / Provincial
 wfs_url = 'https://ide.transporte.gob.ar/geoserver/ows?service=wfs&version=1.3.0&request=GetCapabilities'
+
+# Hospitales privados / publios
+# wfs_url = 'http://ide.sedronar.gov.ar/geoserver/wfs'
+
+# Universidades
+# wfs_url = 'http://mapa.educacion.gob.ar/geoserver/ows?version=1.1.0'
 
 # Connect to GeoServer WFS service.
 wfs = WebFeatureService(wfs_url, version='2.0.0')
 
-print(f'wfs: {wfs}')
-print(f'wfs.contents: {wfs.contents}')
-
-
-keys = dict.keys()
-values = dict.values()
+keys = wfs.contents.keys()
 total_files = len(keys)
 
-print("KEYS")
-for key, value in wfs.contents.items():
-    print(f'{value.title} --> {key}')
+# print("KEYS")
+# for key, value in wfs.contents.items():
+#     print(f'{value.title} --> {key}')
 
 dir = './output2'
 if os.path.exists(dir):

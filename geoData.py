b1144c9dcf3d9fe45b6a188fda69d100809e573f
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-05-09 16:22:22 -0300

    continue working with geo data wfs

--- geoData.py
+++ geoData.py
@@ -2,22 +2,24 @@ import os
 import shutil
 
 from owslib.wfs import WebFeatureService
+from datetime import datetime
 
 # Datos a obtener:
-# Escuelas, universidades, hospitales y centros de salud, policía y bomberos
+# hospitales y centros de salud, policía y bomberos
 # Transporte público, rutas, autopistas, puertos y aeropuertos
 
-# wfs_url = 'http://mapa.educacion.gob.ar/geoserver/ows?service=wfs&version=1.1.0&request=GetCapabilities'
 
-# Transport data
+### Educacion
+wfs_url = 'http://mapa.educacion.gob.ar/geoserver/ows?service=wfs&version=2.0.0&request=GetCapabilities'
+# universidades, establecimientos_educativos
+
+
+### Transport data
 # Colectivos - Estaciones - Puertos - Ferrocarril - Subtes - Ruta Nacional / Provincial
-wfs_url = 'https://ide.transporte.gob.ar/geoserver/ows?service=wfs&version=1.3.0&request=GetCapabilities'
+# wfs_url = 'https://ide.transporte.gob.ar/geoserver/ows?service=wfs&version=1.3.0&request=GetCapabilities'
 
-# Hospitales privados / publios
-# wfs_url = 'http://ide.sedronar.gov.ar/geoserver/wfs'
+### Hospitales privados / publios
 
-# Universidades
-# wfs_url = 'http://mapa.educacion.gob.ar/geoserver/ows?version=1.1.0'
 
 # Connect to GeoServer WFS service.
 wfs = WebFeatureService(wfs_url, version='2.0.0')
@@ -29,7 +31,7 @@ total_files = len(keys)
 # for key, value in wfs.contents.items():
 #     print(f'{value.title} --> {key}')
 
-dir = './output2'
+dir = './output3'
 if os.path.exists(dir):
     shutil.rmtree(dir)
 os.makedirs(dir)
@@ -37,6 +39,9 @@ os.chdir(dir)
 
 print(f'files to create: {total_files}')
 created_num = 0
+
+timeBefore = datetime.now()
+print("Time before:", timeBefore)
 
 for key in sorted(keys):
     print(f'{total_files - created_num} files remaining')
@@ -51,3 +56,11 @@ for key in sorted(keys):
         print(f'Exception in {key}: {error}')
 
     created_num += 1
+
+timeAfter = datetime.now()
+print("Time after:", timeAfter)
+
+timeDiff = timeAfter - timeBefore
+
+value = divmod(timeDiff.total_seconds(), 60)
+print("Minutes: ", value[0], ", Seconds: ", value[1])
